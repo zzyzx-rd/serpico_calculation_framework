5bf0cfa323cd11232fb3fa66c424413c8be21a19
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-06-19 17:15:47 +0200

    tests

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/guillaume/Dropbox/Stage/Stage 2A/serpico_calculation_framework/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

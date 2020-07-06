8ce0aafb7253d107ef02bd89f11968a3493b7380
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-07-06 16:44:07 +0200

    modifs esthétiques

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ def postJsonCriteria():
     @warning doesn't check if the json has the right structure
     @return: a json, with all calculated values
     """
-    print("request json : ", request.get_json(), file=sys.stdout)
     print("request json : ", request.get_json(), file=sys.stderr)
     # check the request has the correct format
     if not request.is_json:
@@ -66,9 +65,3 @@ def postJsonStep():
     print("stepResult : ", result, file=sys.stderr)
     sys.stderr.flush()
     return result
-
-
-@bp.route('/hello', methods=['GET', 'POST'])
-def hello_world():
-    print("Appel de hello : ", file=sys.stdout)
-    return "Welcome on the first version of the framework of result calculation, powered by Serpico"
